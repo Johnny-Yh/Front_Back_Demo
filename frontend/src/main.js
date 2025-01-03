ddb0a1cf9a33d7bb6be08c0a6bc9628381cdad73
Johnny-Yh <1351650904@qq.com>
2025-01-03 14:32:12 +0800

    Replace Vue2 with Vue3

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,4 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.config.productionTip = false
-Vue.use(ElementUI)
-new Vue({
-    render: h => h(App),
-    router
-}).$mount('#app')
+createApp(App).mount('#app')
